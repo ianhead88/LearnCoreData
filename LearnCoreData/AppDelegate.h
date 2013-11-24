684564b4451b0dcf002a694e439a440af100857f
ian <ian@new-host-3.home>
2013-11-24 12:14:33 -0500

    Initial Commit

---
+++ LearnCoreData/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LearnCoreData
+//
+//  Created by ian on 11/24/13.
+//  Copyright (c) 2013 ian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LearnCoreData/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LearnCoreData
+//
+//  Created by ian on 11/24/13.
+//  Copyright (c) 2013 ian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
